b638c0d02def4bf9e1caf4d72f3c7780bde10493
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-20 12:30:28 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,15 +1,37 @@
-variable "name" {
-  description = "Target group name"
-  type        = string
-  default     = "example-tg"  # Example default value
+resource "aws_vpc" "this" {
+  cidr_block = var.cidr_block
+
+  tags = {
+    Name = var.name
+  }
 }
 
-#variable "vpc_id" {
-#  description = "VPC ID"
-#  type        = string
-#}
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
 
-variable "target_id" {
-  description = "Target ID (instance ID)"
-  type        = string
+  tags = {
+    Name = "${var.name}-igw"
+  }
+}
+
+resource "aws_route_table" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "${var.name}-rt"
+  }
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_route_table.this.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+
+output "vpc_id" {
+  value = aws_vpc.this.id
+}
+
+output "route_table_id" {
+  value = aws_route_table.this.id
 }
