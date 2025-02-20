e8a65ae6f8a7f88bcaef7667ac009b7e5a230264
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-20 11:54:17 +0530

    Create main.tf

---
+++ modules/vpc/main.tf
@@ -0,0 +1,15 @@
+variable "name" {
+  description = "Target group name"
+  type        = string
+  default     = "example-tg"  # Example default value
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "target_id" {
+  description = "Target ID (instance ID)"
+  type        = string
+}
