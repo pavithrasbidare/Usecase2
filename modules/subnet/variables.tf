1f877c89ef75e0bc8375ab2a4c0dea44a92c376a
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-20 11:52:51 +0530

    Create variables.tf

---
+++ modules/subnet/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "cidr_block" {
+  description = "CIDR block for the subnet"
+  type        = string
+}
+
+variable "availability_zone" {
+  description = "Availability Zone for the subnet"
+  type        = string
+}
+
+variable "name" {
+  description = "Name tag for the subnet"
+  type        = string
+}
