1983f601b1880f7fbcdfd193ae7f562922fc066a
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-02-20 11:53:52 +0530

    Update variables.tf

--- modules/target_group/variables.tf
+++ modules/target_group/variables.tf
@@ -1,1 +1,16 @@
 
+variable "name" {
+  description = "Target group name"
+  type        = string
+  default     = "example-tg"  # Example default value
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "target_id" {
+  description = "Target ID (instance ID)"
+  type        = string
+}
